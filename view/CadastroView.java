743839015f704d9c52201c5a22645ef43cccb5de
RokaiDeveloper <roger.edab@gmail.com>
2025-03-06 23:46:09 -0300

    Ajustado erro  onde crashava ao terminar 1  cadastro, é só não fechar  o scanner.

--- view/CadastroView.java
+++ view/CadastroView.java
@@ -4,96 +4,95 @@ import java.util.Scanner;
 import model.Carro;
 import model.VeiculoDesc;
 
-
 public class CadastroView {
-    public void MenuCadastro()
-    {
+    public void MenuCadastro() {
         Scanner sc = new Scanner(System.in);
-        {
-            {
-                System.out.println("""
-                                                           Cadastrar veículo
-                                                           =================
-                                                           1. - Carro
-                                                           2. - Moto """);
-                int escolhaCadastro = sc.nextInt();
-                
-                switch (escolhaCadastro) {
-                    case 1:
+        System.out.println("""
+                            Cadastrar veículo
+                            =================
+                            1. - Carro
+                            2. - Moto """);
+        if (sc.hasNextInt()) {
+            int escolhaCadastro = sc.nextInt();
+            sc.nextLine(); // Consumir a nova linha
+
+            switch (escolhaCadastro) {
+                case 1:
                     System.out.println("Carro");
                     Carro carro = new Carro();
-                    
+
                     System.out.println("Para iniciar o cadastro");
                     System.out.println("Informe Marca: ");
                     carro.setMarca(sc.nextLine());
-                    
-                    sc.nextLine();
+
                     System.out.println("Informe Modelo: ");
                     carro.setModelo(sc.nextLine());
-                    
+
                     System.out.print("Informe Ano: ");
-                    carro.setAno(sc.nextInt());
-                    
-                    sc.nextLine();
+                    if (sc.hasNextInt()) {
+                        carro.setAno(sc.nextInt());
+                        sc.nextLine(); // Consumir a nova linha
+                    }
+
                     System.out.print("Informe Cor: ");
                     carro.setCor(sc.nextLine());
-                    
+
                     System.out.println("Informe quilometragem rodado: ");
-                    carro.setKmRodado(sc.nextDouble());
-                    
+                    if (sc.hasNextDouble()) {
+                        carro.setKmRodado(sc.nextDouble());
+                    }
+
                     System.out.print("Informe valor de venda do veículo: ");
-                    carro.setValorVendaVeiculo(sc.nextDouble());
-                    
+                    if (sc.hasNextDouble()) {
+                        carro.setValorVendaVeiculo(sc.nextDouble());
+                    }
+
                     System.out.print("Informe valor fipe de veículo: ");
-                    carro.setValorFipeVeiculo(sc.nextDouble());
-                    
+                    if (sc.hasNextDouble()) {
+                        carro.setValorFipeVeiculo(sc.nextDouble());
+                    }
+
                     VeiculoDesc veiculoDesc = new VeiculoDesc();
                     System.out.println("""
-                                                O veículo possui ar condicionado?
-                                                1. - Sim
-                                                2. - Não
-                                                """);
-                    int arCondicionado = sc.nextInt();
-                    switch (arCondicionado) {
-                        case 1 -> {
-                            veiculoDesc.setArCondicionado(true);
-                            break;
+                                        O veículo possui ar condicionado?
+                                        1. - Sim
+                                        2. - Não
+                                        """);
+                    if (sc.hasNextInt()) {
+                        int arCondicionado = sc.nextInt();
+                        switch (arCondicionado) {
+                            case 1 -> veiculoDesc.setArCondicionado(true);
+                            case 2 -> veiculoDesc.setArCondicionado(false);
+                            default -> System.out.println("Valor inválido");
                         }
-                        case 2 -> {
-                            veiculoDesc.setArCondicionado(false);
-                            break;
-                        }
-                        default -> System.out.println("Valor inválido");
                     }
+
                     System.out.println("""
-                                                O veículo possui direção hidráulica?
-                                                1. - Sim
-                                                2. - Não
-                                                """);
-                    int direcaoHidraulica = sc.nextInt();
-                    switch(direcaoHidraulica) {
-                        case 1 -> {
-                            veiculoDesc.setDirecaoHidraulica(true);
-                            break;
+                                        O veículo possui direção hidráulica?
+                                        1. - Sim
+                                        2. - Não
+                                        """);
+                    if (sc.hasNextInt()) {
+                        int direcaoHidraulica = sc.nextInt();
+                        switch (direcaoHidraulica) {
+                            case 1 -> veiculoDesc.setDirecaoHidraulica(true);
+                            case 2 -> veiculoDesc.setDirecaoHidraulica(false);
+                            default -> System.out.println("Valor inválido");
                         }
-                        case 2 -> {
-                            veiculoDesc.setDirecaoHidraulica(false);
-                            break;
-                        }
-                        default -> System.out.println("Valor inválido");
                     }
                     break;
-                    
-                    case 2:
+
+                case 2:
                     System.out.println("Moto");
                     break;
-                    
-                    default:
-                    sc.close();
-                    throw new AssertionError();
-                }
+
+                default:
+                    System.out.println("Opção inválida");
+                    break;
             }
+        } else {
+            System.out.println("Entrada inválida, por favor insira um número.");
+            sc.next();
         }
-        sc.close();
     }
 }
