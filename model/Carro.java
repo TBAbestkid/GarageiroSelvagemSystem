ccd17bdd4f05d6298178ebff958d8975073d7927
Roger Barreto <140258125+RokaiDeveloper@users.noreply.github.com>
2025-03-10 22:22:09 -0300

    aJUSTADO RETORNO TOSTRING

--- model/Carro.java
+++ model/Carro.java
@@ -10,4 +10,18 @@ public class Carro extends Veiculo{
     public Carro() {
     }
     
+    @Override
+    public String toString() {
+        return String.format("""
+                Carro:
+                Marca: %s
+                Modelo: %s
+                Ano: %d
+                Cor: %s
+                Quilometragem rodado: %.2f km
+                Valor de venda: R$ %.2f
+                Valor FIPE: R$ %.2f
+                %s
+                """, getMarca(), getModelo(), getAno(), getCor(), getKmRodado(), getValorVendaVeiculo(), getValorFipeVeiculo(), getDescOpcional() != null ? getDescOpcional().toString() : "Sem descrição opcional");
+    }
 }
