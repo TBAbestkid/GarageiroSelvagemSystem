232db3c0eccf7240eb36dab8d77554d93553253d
Roger Barreto <140258125+RokaiDeveloper@users.noreply.github.com>
2025-03-10 22:22:32 -0300

    CRIADO METODO DE VALIDAÇÃO DE PLACA QUE FUNCIONA AS AVEZES

--- model/VeiculoDesc.java
+++ model/VeiculoDesc.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class VeiculoDesc {
     private boolean arCondicionado;
     private boolean direcaoHidraulica;
@@ -47,6 +50,27 @@ public class VeiculoDesc {
 
     public void setPlaca(String placa) {
         this.placa = placa;
-    }
 
+    }
+    public Integer validarPlaca() {
+
+        String mercosulPattern = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$";
+        String antigaPattern = "^[A-Z]{3}-[0-9]{4}$";
+
+        Pattern patternMercosul = Pattern.compile(mercosulPattern);
+        Pattern patternAntiga = Pattern.compile(antigaPattern);
+
+        Matcher matcherMercosul = patternMercosul.matcher(this.placa);
+        if (matcherMercosul.matches()) {
+            return 1;
+        }
+
+        Matcher matcherAntiga = patternAntiga.matcher(this.placa);
+        if (matcherAntiga.matches()) {
+            return 1;
+        }
+
+        return 0;
+    }
+    
 }
