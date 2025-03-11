af1ffb5a213cb7c6e914a92f4b0c700039bc2957
Roger Barreto <140258125+RokaiDeveloper@users.noreply.github.com>
2025-03-10 21:35:10 -0300

    Erro do visual studio pois sobrescrevi um commit com o git hard reset e git force

--- Main.java
+++ Main.java
@@ -1,27 +1,52 @@
-import java.util.Scanner;
 import controller.CadastroController;
+import java.util.ArrayList;
+import java.util.Scanner;
+import model.Veiculo;
 
 public class Main {
     public static void main(String[] args) {
+        CadastroController cadastroController = new CadastroController();
+        ArrayList<Veiculo> veiculos = new ArrayList<>(); 
         Scanner scanner = new Scanner(System.in);
         boolean menuAtivo = true;
         while (menuAtivo) {
             System.out.println("""
                                 Menu
-                                ====
+                                =============
                                 1. - Cadastro
-                                2. - Sair
+                                2. - Edição
+                                3. - Exclusão
+                                4. - Listagem
+                                5. - Sair
                                 """);
             if (scanner.hasNextInt()) {
                 int escolha = scanner.nextInt();
                 switch (escolha) {
                     case 1:
-                        CadastroController cadastroController = new CadastroController();
+
                         cadastroController.MenuCadastro();
                         break;
+
                     case 2:
-                        menuAtivo = false;
+                        System.out.println("");
                         break;
+                
+                    case 3:
+                        System.out.println("Edição");
+                        break;
+
+                    case 4:
+                        System.out.println("Exclusão");
+                        break;
+                    
+                    case 5:
+                        System.out.println("Listagem");
+                        break;
+
+                    case 6:
+                        System.out.println("Sair");
+                        break;
+
                     default:
                         System.out.println("Opção inválida");
                         break;
