06620edfb32a9c439392f1c77105a757615d779e
Kharen Aguiar <kharenaguiar@gmail.com>
2025-03-12 15:04:39 -0300

    Inserindo ToString em Moto
    DE NOVO INSERINDO O TOSTRING DE MOTO NO MODELO

--- model/Moto.java
+++ model/Moto.java
@@ -11,4 +11,20 @@ public class Moto extends Veiculo {
     {
         super();
     }
+
+    @Override
+    public String toString(){
+        return String.format("""
+            Moto
+            Marca: %s
+            Modelo: %s
+            Ano: %d
+            Cor: %s
+            Quilometragem rodado: %.2f km
+            Valor de venda: R$ %.2f
+            Valor FIPE: R$ %.2f
+            %s
+            """, getMarca(), getModelo(), getAno(), getCor(), getKmRodado(), getValorVendaVeiculo(), getValorFipeVeiculo(), getDescOpcional() != null ? getDescOpcional().toString() : "Sem descrição opcional");    
+    }
+
 }
