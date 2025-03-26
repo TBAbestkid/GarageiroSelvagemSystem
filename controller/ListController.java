710ed7b2528b7a18d54c1a7b0c2804b658ab53ba
Kharen Aguiar <kharenaguiar@gmail.com>
2025-03-26 15:14:45 -0300

    Adicionando comentarios explicativos no controller de listagem

--- controller/ListController.java
+++ controller/ListController.java
@@ -5,13 +5,19 @@ import model.Veiculo;
 import view.ListView;
 
 public class ListController {
+
+    // Lista de listas de veículos, permitindo gerenciar múltiplos grupos de
+    // veículos
     private List<List<Veiculo>> listaDeListas;
+
+    // Agregação: ListController mantém uma referência a ListView
     private ListView listView = new ListView();
 
     public ListController(List<List<Veiculo>> listaDeListas) {
         this.listaDeListas = listaDeListas;
     }
 
+    // Método que chama a View para exibir a lista de veículos
     public void MenuList() {
         listView.MenuList(listaDeListas);
     }
