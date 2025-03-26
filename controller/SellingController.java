8606ed0fd0da5e15e1ab5fa25ef596a86e1275cc
Kharen Aguiar <kharenaguiar@gmail.com>
2025-03-26 15:15:02 -0300

    adicionando comentarios explicativos no controller de venda

--- controller/SellingController.java
+++ controller/SellingController.java
@@ -5,13 +5,15 @@ import model.Veiculo;
 import view.SellView;
 
 public class SellingController {
-    
+
+    // Agregação: SellingController mantém uma referência a SellView
     private final SellView sellview;
 
-    public SellingController(){
+    public SellingController() {
         this.sellview = new SellView();
     }
-    
+
+    // Método que chama a View para exibir o menu de venda de veículos
     public List<List<Veiculo>> MenuSell(List<List<Veiculo>> listaDeListas) {
         return sellview.MenuSell(listaDeListas);
     }
