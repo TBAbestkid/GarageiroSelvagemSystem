8b3d03d9e4479d3141d1fccfa8e440ec08339638
Kharen Aguiar <kharenaguiar@gmail.com>
2025-03-26 15:14:05 -0300

    Adicionando comentarios explicativos no controller de cadastro

--- controller/CadastroController.java
+++ controller/CadastroController.java
@@ -5,15 +5,20 @@ import model.Veiculo;
 import view.CadastroView;
 
 public class CadastroController {
-    
+
+    // Agregação: CadastroController precisa de um CadastroView
+    // mas não o cria dentro de um método
+
     private final CadastroView cadastroView;
 
     public CadastroController() {
+        // A instância do CadastroView é passada para este controller
         this.cadastroView = new CadastroView();
     }
 
-    public List<Veiculo> MenuCadastro()
-    {
-         return cadastroView.MenuCadastro();
+    // Método que chama a view para exibir
+    // menu de cadastro e retorna a lista de veículos cadastrados
+    public List<Veiculo> MenuCadastro() {
+        return cadastroView.MenuCadastro();
     }
 }
