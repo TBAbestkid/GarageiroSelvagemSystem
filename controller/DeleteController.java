1cb41f73a5989de2ad0af8a2dc0da8b88e2edf10
Kharen Aguiar <kharenaguiar@gmail.com>
2025-03-26 15:14:26 -0300

    Adicionando comentarios explitacivos no controller delete

--- controller/DeleteController.java
+++ controller/DeleteController.java
@@ -5,14 +5,16 @@ import model.Veiculo;
 import view.DeleteView;
 
 public class DeleteController {
-    
+
+    // Agregação: DeleteController mantém uma instância de DeleteView
     private final DeleteView deleteView;
 
     public DeleteController() {
         this.deleteView = new DeleteView();
     }
 
+    // Método que chama a view para exibir o menu de exclusão de veículos
     public List<List<Veiculo>> MenuDelete(List<List<Veiculo>> listaDeListas) {
-         return deleteView.MenuDelete(listaDeListas);
+        return deleteView.MenuDelete(listaDeListas);
     }
 }
